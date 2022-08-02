0f4bc21f2e74b6e584f79e8cb6ba11c013e2d22f
KennSecusana <kennsecusana@gmail.com>
2022-08-02 18:22:41 +0800

    Final Commit

--- resources/js/pages/Service.vue
+++ resources/js/pages/Service.vue
@@ -2,19 +2,19 @@
 <h1 class="text-center" style="font-size:40px; color: dimgray;">Available Buses</h1>
 
 <div class="d-flex align-contents-between" style="position:relative; left:150px; ">
-  <div class="card mb-3" style="margin-left:20px;" v-for="booking in services" :key="booking.id">
+  <div class="card mb-3 bg-secondary" style="margin-left:20px;" v-for="booking in services" :key="booking.id">
      <div class="card-header bg-success">
-        <h3 style="font-size:20px;">{{booking.bus_name}}</h3>        
+        <h3 style="font-size:20px; color: white;">{{booking.bus_name}}</h3>        
      </div>
         
           <div class="card-body" style="width: 200px;">
           
           </div>
           <ul>
-            <li v-for="capacity in booking.services" :key="capacity.id"><h5 style="color:dimgray;">Capacity</h5><br>
-            <h4 style="font-size:15px;">Maximum Capacity:</h4> {{capacity.maximum_seats}}  <br><br>
-            <h4 style="font-size:15px;">Plate No:</h4> {{capacity.plate_no}} <br><br>
-            <h4 style="font-size:15px;">Franchise No:</h4>  {{capacity.franchise_no}}
+            <li v-for="capacity in booking.services" :key="capacity.id"><h5 style="color:white;">Capacity</h5><br>
+            <h4 style="font-size:15px; color: white;">Maximum Capacity:</h4> {{capacity.maximum_seats}}  <br><br>
+            <h4 style="font-size:15px; color: white;">Plate No:</h4> {{capacity.plate_no}} <br><br>
+            <h4 style="font-size:15px; color: white;">Franchise No:</h4>  {{capacity.franchise_no}}
             </li>
           </ul>
   </div>
